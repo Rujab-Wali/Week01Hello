ee68ffa7d2e636a61b108d77f53c0bce170e5396
Rujab <233826543+Rujab-Wali@users.noreply.github.com>
2025-10-08 10:13:51 +0500

    Add files via upload

---
+++ Week03-Hometask02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class AgeDiscount
+{
+    static void Main()
+    {
+        Console.Write("Enter age: ");
+        int age = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Are you a student? (Y/N): ");
+        string student = Console.ReadLine()!.Trim().ToUpper();
+
+        double price = 100;
+        double discount = 0;
+
+        if (age < 12)
+            discount = 0.5;
+        else if (student == "Y")
+            discount = 0.25;
+        else if (age >= 60)
+            discount = 0.3;
+        else
+            discount = 0;
+
+        double final = price - (price * discount);
+
+        Console.WriteLine($"Base price : {price}");
+        Console.WriteLine($"Discount   : {discount * 100}%");
+        Console.WriteLine($"Final price: {final}");
+    }
+}
