fa6c6b3cd06dc41d23f93387fe2b6413cf9f0c4f
Rujab <233826543+Rujab-Wali@users.noreply.github.com>
2025-10-11 23:59:31 +0500

    Add files via upload

---
+++ Week03-Hometask03.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class StringLengthChecker
+{
+    static void Main()
+
+    {
+        Console.WriteLine("Enter any text:");
+        string input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+            Console.WriteLine("Empty String");
+        else if (string.IsNullOrWhiteSpace(input))
+            Console.WriteLine("WhiteSpace String");
+        else if (input.Length < 5)
+            Console.WriteLine("Short String");
+        else if (input.Length > 20)
+            Console.WriteLine("Long String");
+        else
+            Console.WriteLine("Normal Length string");
+    }
+}
+
