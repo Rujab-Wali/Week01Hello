d34bce36054779d727d76080170f208352337582
Rujab <233826543+Rujab-Wali@users.noreply.github.com>
2025-10-13 10:39:35 +0500

    Add files via upload

---
+++ Week04-Exercise01-Challange02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+enum TrafficLight { Red, Yellow, Green }
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            foreach (TrafficLight light in Enum.GetValues(typeof(TrafficLight)))
+            {
+                Console.WriteLine($"\nCurrent light: {light}");
+
+                if (light == TrafficLight.Red)
+                    Console.WriteLine("STOP! Pedestrians may cross.");
+                else if (light == TrafficLight.Yellow)
+                    Console.WriteLine("Prepare to stop. Pedestrians wait.");
+                else
+                    Console.WriteLine("GO! Pedestrians must wait.");
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
